0ef250630dbdd72263c9da0268ed0349c909d852
Aaron Rioflorido <aaron.rioflorido@gmail.com>
2025-08-23 09:57:05 +0800

    chore: update todos
    Signed-off-by: Aaron Rioflorido <aaron.rioflorido@gmail.com>

--- index.js
+++ index.js
@@ -145,3 +145,5 @@ operateButton.addEventListener("click", () => {
 // 4) convert all of this into a Calculator Class
 // 5) Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator
 // 6) When a result is displayed, pressing a new digit should clear the result and start a new calculation instead of appending the digit to the existing result. Check whether this is the case on your calculator!
+// 7) Add a "backspace" button, so the user can undo their last input if they click the wrong number.
+// 8) Add keyboard support!
